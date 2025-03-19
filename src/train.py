684f9f60f2e5c06a482ac78c171e7c11d528f1f5
Gauravjangid26 <gauravjangid341542@gmail.com>
2025-03-19 15:28:16 +0530

    pylint

--- src/train.py
+++ src/train.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import joblib
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import LabelEncoder
